b2ed3d372b8b979505eee7d849f54d0bffab97df
Tran Nhat Tien <103252635+nhattien2123@users.noreply.github.com>
2023-12-09 23:44:45 +0700

    fix sidebar UI

--- client/src/share/sidebar/Sidebar.tsx
+++ client/src/share/sidebar/Sidebar.tsx
@@ -6,6 +6,7 @@ import { userActions } from "src/redux/reducer/user/UserReducer";
 import { useAppDispatch, useAppSelector } from "src/app/hooks";
 import { RootState } from "src/app/store";
 import { socket } from "src/index";
+import logoIcon from "src/assets/images/chess-realm-logo.png";
 import "src/share/sidebar/Sidebar.scss";
 
 interface Props { }
@@ -53,19 +54,19 @@ const Sidebar = (props: Props) => {
     //     }
     // });
 
-    const [isSidebarOpen, setSidebarOpen] = useState(true);
-    const [isDarkMode, setDarkMode] = useState(false);
+    const [isSidebarClosed, setIsSidebarClosed] = useState(true);
+    const [isDarkMode, setIsDarkMode] = useState(false);
 
     const toggleSidebar = () => {
-        setSidebarOpen(!isSidebarOpen);
+        setIsSidebarClosed(!isSidebarClosed);
     };
 
-    const openSidebar = () => {
-        setSidebarOpen(false);
+    const openSearchBox = () => {
+        setIsSidebarClosed(false);
     };
 
     const toggleDarkMode = () => {
-        setDarkMode(!isDarkMode);
+        setIsDarkMode(!isDarkMode);
     };
 
     return (
@@ -202,78 +203,98 @@ const Sidebar = (props: Props) => {
                 </div>
             </nav>
             <script></script> */}
-            <nav className={`sidebar${isSidebarOpen ? '' : 'close'}${isDarkMode ? 'dark' : ''}`}>
+            <nav className={`sidebar ${isSidebarClosed ? 'close' : ''}`}>
                 <header>
-                    <div className="image-text">
-                        <span className="image">
-                            <img src="src/assets/images/chess-realm-logo.png" alt="logo" />
-                        </span>
-
-                        <div className="text header-text">
-                            <span className="name">CodingLab</span>
-                            <span className="profession">Web developer</span>
+                    <Link to={"/"}>
+                        <div className="image-text">
+                            <span className="image">
+                                <img src={logoIcon} alt="Logo Chess Realm" />
+                            </span>
+                            <div className="text logo-text">
+                                <span className="name">Chess Realm</span>
+                                <span className="profession">Made by SupportX</span>
+                            </div>
                         </div>
-                    </div>
-
-                    <i className="bx bx-chevron-right toggle" onClick={toggleSidebar}></i>
+                    </Link>
+                    <i className='bx bx-chevron-right toggle' onClick={toggleSidebar}></i>
                 </header>
                 <div className="menu-bar">
                     <div className="menu">
-                        <li className="search-box">
-                            <i className="bx bx-search icon"></i>
-                            <input type="search" placeholder="Search..." />
+                        <li className="search-box" onClick={openSearchBox}>
+                            <i className='bx bx-search icon'></i>
+                            <input
+                                placeholder="Tìm kiếm..."
+                                onChange={(evt) => setKw(evt.target.value)}
+                                onKeyDown={(evt) => {
+                                    if (evt.key === "Enter") {
+                                        if (kw.trim() !== "") {
+                                            nav(`/player/?kw=${kw}`);
+                                        }
+                                    }
+                                }}
+                            ></input>
                         </li>
                         <ul className="menu-links">
                             <li className="nav-link">
-                                <a href="#">
-                                    <i className="bx bx-home-alt icon"></i>
-                                    <span className="text nav-text">Dashboard</span>
-                                </a>
+                                <Link to={"/play/online"} className="nav-link-contain">
+                                    <i className="fa-solid fa-chess-board"></i>
+                                    <span className="text nav-text">Play</span>
+                                </Link>
                             </li>
                             <li className="nav-link">
-                                <a href="#">
-                                    <i className="bx bx-home-alt icon"></i>
-                                    <span className="text nav-text">Dashboard</span>
-                                </a>
+                                <Link to={`/profile/${currentUser.username}`} className="nav-link-contain">
+                                    <i className='bx bx-bell icon'></i>
+                                    <span className="text nav-text">Information</span>
+                                </Link>
                             </li>
                             <li className="nav-link">
-                                <a href="#">
-                                    <i className="bx bx-home-alt icon"></i>
-                                    <span className="text nav-text">Dashboard</span>
-                                </a>
+                                <Link to={"/messages"} className="nav-link-contain">
+                                    <i className='bx bx-bell icon'></i>
+                                    <span className="text nav-text">Message</span>
+                                </Link>
                             </li>
-                            <li className="nav-link">
-                                <a href="#">
-                                    <i className="bx bx-home-alt icon"></i>
-                                    <span className="text nav-text">Dashboard</span>
-                                </a>
-                            </li>
-                            <li className="nav-link">
-                                <a href="#">
-                                    <i className="bx bx-home-alt icon"></i>
-                                    <span className="text nav-text">Dashboard</span>
-                                </a>
-                            </li>
+                            {currentUser.role === "ADMIN" && (
+                                <li className="nav-link">
+                                    <Link to={"/admin"} className="nav-link-contain">
+                                        <i className='bx bx-bell icon'></i>
+                                        <span className="text nav-text">Management</span>
+                                    </Link>
+                                </li>
+                            )}
                         </ul>
                     </div>
-
                     <div className="bottom-content">
                         <li className="">
-                            <a href="#">
-                                <i className="bx bx-log-out icon"></i>
-                                <span className="text nav-text">Logout</span>
-                            </a>
+                            {currentUser._id === "" ? (
+                                <>
+                                    <li className="sidebar-btn">
+                                        <Link to={`/register`} className="btn-form w-80 nav-link-contain">
+                                            Đăng ký
+                                        </Link>
+                                    </li>
+                                    <li className="sidebar-btn">
+                                        <Link to={`/login`} className="btn-form btn-form-save w-80 nav-link-contain">
+                                            Đăng nhập
+                                        </Link>
+                                    </li>
+                                </>
+                            ) : (
+                                <>
+                                    <Link to={`/login`} className="btn-form w-80 btn-form-save nav-link-contain" onClick={logOut}>
+                                        <i className='bx bx-log-out icon'></i>
+                                        <span className="text nav-text">Logout</span>
+                                    </Link>
+                                </>
+                            )}
                         </li>
-
-                        <li className="mode">
-                            <div className="moon-sun">
-                                <i className="bx bx-moon icon"></i>
-                                <i className="bx bx-sun icon"></i>
+                        <li className="mode" onClick={toggleDarkMode}>
+                            <div className="sun-moon">
+                                <i className={`bx ${isDarkMode ? 'bx-sun' : 'bx-moon'} icon moon`}></i>
+                                <i className={`bx ${isDarkMode ? 'bx-moon' : 'bx-sun'} icon sun`}></i>
                             </div>
-                            <span className="mode-text text">{isDarkMode ? 'Dark Mode' : 'Light Mode'}</span>
-
-                            <div className="toggle-switch" onClick={toggleDarkMode}>
-                                <span className={`switch ${isDarkMode ? 'on' : 'off'}`}></span>
+                            <span className="mode-text text">{isDarkMode ? 'Light mode' : 'Dark mode'}</span>
+                            <div className="toggle-switch">
+                                <span className="switch"></span>
                             </div>
                         </li>
                     </div>
