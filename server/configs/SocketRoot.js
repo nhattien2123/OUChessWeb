76a510e75d85899e4a66b3836945d01dc721cf56
Tran Ngoc Tuan <tranngoctuan20102002@gmail.com>
2024-04-14 02:27:49 +0700

    [update] new socket

--- server/configs/SocketRoot.js
+++ server/configs/SocketRoot.js
@@ -1,61 +1,178 @@
+const crypto = require('crypto');
+
 const rootSocket = (io) => {
-    let userConnected = {};
-    const jwt = require("jsonwebtoken");
-    let user = 0;
+    let userConnected = [];
+    let rooms = new Array();
+    const jwt = require('jsonwebtoken');
+    let userCount = 0;
 
     io.use((socket, next) => {
         const token = socket.handshake.auth.token;
         if (token) {
             jwt.verify(token, process.env.JWT_SECRETKEY, (err, user) => {
                 if (err) {
-                    console.log("Lỗi");
+                    return;
                 } else {
+                    socket.token = token;
                     socket.userId = user._id;
-                    userConnected[user._id] = socket.id;
+                    userConnected[user._id] = {
+                        detail: { ...socket.handshake.auth.userInfo },
+                        socket: socket.id,
+                    };
                 }
             });
-            socket.token = token;
+        } else {
+            return;
         }
 
         next();
     });
 
-    io.on("connection", (socket) => {
-        user += 1;
-        console.log("connection - (con): ", userConnected);
+    io.on('connection', (socket) => {
+        userCount += 1;
+        console.log('connection - (con): ', userConnected);
+        console.log('rooms: ', rooms);
 
-        // socket.use(([event, ...arg], next) => {
-        //     if (socket.token) {
-        //         next();
-        //     }
-        // });
-
-        socket.on("disconnect", () => {
-            user -= 1;
-            console.log("connection (dis): ", userConnected);
+        socket.on('disconnect', () => {
+            userCount -= 1;
             if (socket.userId) {
                 delete userConnected[socket.userId];
             }
+            console.log('connection (dis): ', userConnected);
         });
 
-        require("../services/CommentInfoService").commentInfoSocket(socket, io, (error) => {
-            socket.emit("error_msg", { error });
+        socket.on('reconnect', () => {});
+        require('../services/CommentInfoService').commentInfoSocket(socket, io, (error) => {
+            socket.emit('error_msg', { error });
         });
 
-        require("../services/FriendService").friendSocket(socket, io, userConnected);
+        require('../services/FriendService').friendSocket(socket, io, userConnected);
+
+        //#region new socket
+        // on
+        // - type: new, join: required.
+        // - rID: if type = join.
+        // - title: if type = new.
+        // - id: required.
+        // emit
+        // - status: 1 Sucess / 2 Full / 3 Fail.
+        // - roomDetail: id, player list, title,
+        // - player color: white/black
+        socket.on('join-room', async (detail) => {
+            try {
+                if (detail.type === 'new') {
+                    let rID = crypto.randomBytes(2).toString('hex');
+                    while (rooms.filter((r) => r.id === rID).length > 0) {
+                        rID = crypto.randomBytes(2).toString('hex');
+                    }
+
+                    const player = new Array();
+                    player.push(userConnected[socket.userId].detail);
+                    const room = {
+                        id: rID,
+                        title: detail.title,
+                        player: player,
+                    };
+
+                    rooms.push(room);
+                    socket.join(room.id);
+                    socket.emit('rep-join-room', {
+                        detail: room,
+                        status: 1,
+                        color: 0,
+                    });
+                    console.log('Player create');
+                    io.expert(rooms).emit('req-get-rooms', rooms);
+                } else if (detail.type === 'join') {
+                    const room = rooms.filter((r) => r.id === detail.rID)[0];
+                    if (room.player.length === 2) {
+                        socket.emit('rep-join-room', {
+                            detail: null,
+                            status: 2,
+                            color: null,
+                        });
+                    } else {
+                        room.player.push(userConnected[socket.userId].detail);
+                        rooms[rooms.findIndex((r) => r.id === room.id)] = room;
+                        socket.join(room.id);
+                        io.to(room.id).emit('rep-join-room', {
+                            detail: room,
+                            status: 1,
+                            color: 1,
+                        });
+                        console.log(socket.rooms);
+
+                        io.expert(rooms).emit('req-get-rooms', rooms);
+                    }
+                } else {
+                    socket.emit('rep-join-room', {
+                        detail: null,
+                        status: 3,
+                        color: null,
+                    });
+                }
+            } catch (ex) {
+                socket.emit('rep-create-room', {
+                    detail: null,
+                    status: 3,
+                    color: null,
+                });
+            }
+
+            console.log(rooms);
+        });
+
+        socket.on('get-rooms', async () => {
+            console.log(rooms);
+            socket.emit('rep-get-rooms', rooms);
+        });
+
+        // on
+        // - roomID
+        socket.on('leave-room', async (request) => {
+            const room = rooms[request.roomID];
+            room.players.remove((p) => p.player._id === socket.userId);
+            io.to(room.id).emit('req-leave-room');
+            socket.leave(room.id);
+            console.log('Player left');
+
+            if (room.players.length === 0) {
+                rooms.remove(room.id);
+                io.emit('req-get-rooms', rooms);
+            }
+        });
+
+        // on
+        // - start
+        // - target
+        // - flag
+        // - promo
+        socket.on('send-move', async (request) => {
+            const { rId, moving, timer } = request;
+
+            io.to(rId).emit('req-send-move', {
+                moving: moving,
+                timer: timer,
+            });
+        });
+
+        //#endregion new socket
+
+        //#region old socket
         socket.on(`existingPlayer`, (data) => {
-            io.sockets.in(data.roomId).emit(`clientExistingPlayer`, data)
-        })
+            io.sockets.in(data.roomId).emit(`clientExistingPlayer`, data);
+        });
 
-        require("../services/GameService").cameraMove(socket, io);
-        require("../services/GameService").disconnect(socket, io);
-        require("../services/GameService").leaveRoom(socket, io);
-        require("../services/GameService").fetchPlayers(socket, io);
-        require("../services/GameService").joinRoom(socket, io);
-        require("../services/GameService").makeMove(socket, io);
-        require("../services/GameService").resetGame(socket, io);
-        require("../services/GameService").sendMessage(socket, io);
-        require("../services/GameService").promotePawn(socket, io);
+        require('../services/GameService').cameraMove(socket, io);
+        require('../services/GameService').disconnect(socket, io);
+        require('../services/GameService').leaveRoom(socket, io);
+        require('../services/GameService').fetchPlayers(socket, io);
+        require('../services/GameService').joinRoom(socket, io);
+        require('../services/GameService').makeMove(socket, io);
+        require('../services/GameService').resetGame(socket, io);
+        require('../services/GameService').sendMessage(socket, io);
+        require('../services/GameService').promotePawn(socket, io);
+        //#endregion old socket
     });
 };
 
