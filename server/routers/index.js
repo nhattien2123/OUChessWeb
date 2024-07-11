743b23175e71662fe9fd7eee406a47aa2172ae47
Tran Ngoc Tuan <tranngoctuan20102002@gmail.com>
2024-07-11 16:23:46 +0700

    update import 5

--- server/routers/index.js
+++ server/routers/index.js
@@ -5,5 +5,7 @@ router.use("/auth", require("./auth"));
 router.use("/user", require("./user"));
 router.use("/match", require("./match"));
 router.use("/admin", require("./admin"))
-
+router.get("/", () => {
+    console.log("Hello");
+})
 module.exports = router;
